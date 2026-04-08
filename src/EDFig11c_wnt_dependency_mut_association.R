e480ceba1ba3232b29c1bfb4519a3bbf709c01e5
smaffa <sammaffa@gmail.com>
2026-04-08 02:22:24 -0400

    update tabular output format for statistical summaries

--- src/EDFig11c_wnt_dependency_mut_association.R
+++ src/EDFig11c_wnt_dependency_mut_association.R
@@ -6,20 +6,51 @@ library(ggplot2)
 
 RepMut_Org<-read.csv(file.path(INPUT_PATH, "RepMut_Org.csv"))
 WntTop5_Org<-read.csv(file.path(INPUT_PATH, "WntTop5_Org.csv"))
+oncokb_annot_df <- read.csv(file.path(INPUT_PATH, "OncoKB_mutant_class.csv"))[c('Mut', 'oncoKB', 'class')]
 
-if (file.exists(file.path(OUTPUT_PATH, "WntTop5_MutCorr.txt"))) {file.remove(file.path(OUTPUT_PATH, "WntTop5_MutCorr.txt"))}
+if (file.exists(file.path(OUTPUT_PATH, "WntTop5_MutCorr.csv"))) {file.remove(file.path(OUTPUT_PATH, "WntTop5_MutCorr.csv"))}
 
+wnt_genes <- c()
+mut_strings <- c()
+mut_genes <- c()
+mut_types <- c()
+mean_wts <- c()
+mean_muts <- c()
+tps <- c()
+mwups <- c()
 for(i in 6:10) {
   for (j in 4:ncol(RepMut_Org)){
+    wnt_genes <- c(wnt_genes, colnames(WntTop5_Org)[i])
+    mut_string <- colnames(RepMut_Org)[j]
+    mut_string <- sub('HLA\\.A', 'HLA-A', mut_string)
+    mut_string <- sub('HLA\\.B', 'HLA-B', mut_string)
+    mut_strings <- c(mut_strings, mut_string)
+    mut_genes <- c(mut_genes, strsplit(mut_string, "_")[[1]][1]) 
+    mut_types <- c(mut_types, strsplit(mut_string, "_")[[1]][2]) 
+    
     mwut<-wilcox.test(WntTop5_Org[,i]~RepMut_Org[,j])
     mwup<-mwut$p.value
     ttest<-t.test(WntTop5_Org[,i]~RepMut_Org[,j])
     ttese<-ttest$estimate
     ttesp<-ttest$p.value
-    cat(i,j,ttese,ttesp,mwup,"\n",file=file.path(OUTPUT_PATH, "WntTop5_MutCorr.txt"),append=TRUE)
+    mean_wts <- c(mean_wts, ttese['mean in group FALSE'])
+    mean_muts <- c(mean_muts, ttese['mean in group TRUE'])
+    tps <- c(tps, ttesp)
+    mwups <- c(mwups, mwup)
   }
 }
+ttest_df <- data.frame(list(WNT=wnt_genes,MutGene=mut_genes,MutType=mut_types,Mut=mut_strings,
+                            mean_WT=mean_wts,mean_MUT=mean_muts))
+ttest_df['MUT-WT'] <- ttest_df$mean_MUT - ttest_df$mean_WT
+ttest_df$t_p <- tps
+ttest_df$MWU_p <- mwups
+ttest_df$log10P <- -log10(ttest_df$MWU_p)
+ttest_df$MWU_q <- p.adjust(ttest_df$MWU_p, method='BH')
+ttest_df <- merge(ttest_df, oncokb_annot_df, by='Mut')
+ttest_df <- ttest_df[c('WNT', 'MutGene', 'MutType', 'Mut', 'mean_WT', 'mean_MUT', 'MUT-WT', 't_p', 'MWU_p', 'log10P', 'MWU_q', 'oncoKB', 'class')]
+ttest_df <- ttest_df[order(ttest_df$MWU_p, ttest_df$class), ]
+write.csv(ttest_df, file.path(OUTPUT_PATH, "WntTop5_MutCorr.csv"), quote=FALSE, row.names=FALSE)
 
-WntTop5_MutCorr<-read.csv(file.path(INPUT_PATH, "WntTop5_MutCorr.csv"))
+WntTop5_MutCorr<-read.csv(file.path(OUTPUT_PATH, "WntTop5_MutCorr.csv"))
 
 ggplot(WntTop5_MutCorr,aes(x=MUT.WT,y=log10P,colour=class))+geom_point(alpha=0.6, size=3)+theme(aspect.ratio=1)+scale_color_manual(values=c("#bbbbbb","#931751","#005492"))
--- src/EDFig12ac_2d3d_onctsg_dependency.R
+++ src/EDFig12ac_2d3d_onctsg_dependency.R
@@ -7,52 +7,90 @@ library(ggplot2)
 mONCTSG_Gene<-read.csv(file.path(INPUT_PATH, "mONCTSG_Gene.csv")) 
 mONCTSG_MutFree<-read.csv(file.path(INPUT_PATH, "mONCTSG_MutFree.csv")) 
 mONCTSG_Pathway<-read.csv(file.path(INPUT_PATH, "mONCTSG_Pathway.csv")) 
+onc_tsg_gene_annot <- read.csv(file.path(INPUT_PATH, "OncTSG_annotations.csv"), row.names = 1)
 
-if (file.exists(file.path(OUTPUT_PATH, "mONCTSG_Gene_Result.txt"))) {file.remove(file.path(OUTPUT_PATH, "mONCTSG_Gene_Result.txt"))}
-if (file.exists(file.path(OUTPUT_PATH, "mONCTSG_MutFree_Result.txt"))) {file.remove(file.path(OUTPUT_PATH, "mONCTSG_MutFree_Result.txt"))}
-if (file.exists(file.path(OUTPUT_PATH, "mONCTSG_Pathway_Result.txt"))) {file.remove(file.path(OUTPUT_PATH, "mONCTSG_Pathway_Result.txt"))}
-if (file.exists(file.path(OUTPUT_PATH, "mOT_gene_q.csv"))) {file.remove(file.path(OUTPUT_PATH, "mOT_gene_q.csv"))}
-if (file.exists(file.path(OUTPUT_PATH, "mOT_mutfree_q.csv"))) {file.remove(file.path(OUTPUT_PATH, "mOT_mutfree_q.csv"))}
-if (file.exists(file.path(OUTPUT_PATH, "mOT_pathway_q.csv"))) {file.remove(file.path(OUTPUT_PATH, "mOT_pathway_q.csv"))}
+if (file.exists(file.path(OUTPUT_PATH, "mONCTSG_Gene_Result2.csv"))) {file.remove(file.path(OUTPUT_PATH, "mONCTSG_Gene_Result2.csv"))}
+if (file.exists(file.path(OUTPUT_PATH, "mONCTSG_Pathway_Result.csv"))) {file.remove(file.path(OUTPUT_PATH, "mONCTSG_Pathway_Result.csv"))}
 
+mean_2ds <- c()
+mean_3do <- c()
+tps <- c()
+mwups <- c()
 for(i in 5:ncol(mONCTSG_Gene)) {
   mwut<-wilcox.test(mONCTSG_Gene[,i]~mONCTSG_Gene[,4])
   mwup<-mwut$p.value
   ttest<-t.test(mONCTSG_Gene[,i]~mONCTSG_Gene[,4])
   tteste<-ttest$estimate
   ttesp<-ttest$p.value
-  cat(i,tteste,ttesp,mwup,"\n",file=file.path(OUTPUT_PATH, "mONCTSG_Gene_Result.txt"), append=TRUE)
+  mean_2ds <- c(mean_2ds, tteste['mean in group 2DS'])
+  mean_3do <- c(mean_3do, tteste['mean in group 3DO'])
+  tps <- c(tps, ttesp)
+  mwups <- c(mwups, mwup)
 }
+ttest_df <- data.frame(list(mean_2DS=mean_2ds,mean_3DO=mean_3do))
+ttest_df$t_p <- tps
+ttest_df$MWU_p <- mwups
+ttest_df$log10P <- -log10(ttest_df$MWU_p)
+ttest_df$MWU_q <- p.adjust(ttest_df$MWU_p, method='BH')
+rownames(ttest_df) <- colnames(mONCTSG_Gene)[5:ncol(mONCTSG_Gene)]
+ttest_df <- ttest_df[ttest_df$MWU_q < 0.05, ]
+write.csv(ttest_df, file.path(OUTPUT_PATH, "mONCTSG_Gene_Result2.csv"), quote=FALSE)
 
+mutfree_p <- c()
 for(i in 5:ncol(mONCTSG_MutFree)) {
   mwut<-wilcox.test(mONCTSG_MutFree[,i]~mONCTSG_MutFree[,4])
   mwup<-mwut$p.value
-  ttest<-t.test(mONCTSG_MutFree[,i]~mONCTSG_MutFree[,4])
-  tteste<-ttest$estimate
-  ttesp<-ttest$p.value
-  cat(i,tteste,ttesp,mwup,"\n",file=file.path(OUTPUT_PATH, "mONCTSG_MutFree_Result.txt"), append=TRUE)
+  mutfree_p <- c(mutfree_p, mwup)
 }
+mutfree_df <- data.frame(list(mutfree_p=mutfree_p, mutfree_q=p.adjust(mutfree_p, method='BH')))
+mutfree_df$mf_confirmation <- mutfree_df$mutfree_q < 0.05
+rownames(mutfree_df) <- colnames(mONCTSG_MutFree)[5:ncol(mONCTSG_MutFree)]
 
+ttest_df <- merge(ttest_df, mutfree_df, by='row.names')
+ttest_df <- data.frame(ttest_df[, -1], row.names = ttest_df[, "Row.names"])
+ttest_df <- merge(ttest_df, onc_tsg_gene_annot, by='row.names')
+ttest_df <- data.frame(ttest_df[, -1], row.names = ttest_df[, "Row.names"])
+ttest_df$significance <- ttest_df$MWU_q < 0.05
+ttest_df$class <- ifelse(ttest_df$significance, 'significant', 'insignificant')
+ttest_df <- ttest_df[c('oncTSG', 'mean_2DS', 'mean_3DO', 't_p', 'MWU_p', 'log10P', 'MWU_q', 'mutfree_q', 'significance', 'mf_confirmation', 'class')]
+ttest_df <- ttest_df[order(ttest_df$MWU_q), ]
+write.csv(ttest_df, file.path(OUTPUT_PATH, "mONCTSG_Gene_Result2.csv"), quote=FALSE)
+
+
+mean_2ds <- c()
+mean_3do <- c()
+tps <- c()
+mwups <- c()
 for(i in 5:ncol(mONCTSG_Pathway)) {
   mwut<-wilcox.test(mONCTSG_Pathway[,i]~mONCTSG_Pathway[,4])
   mwup<-mwut$p.value
   ttest<-t.test(mONCTSG_Pathway[,i]~mONCTSG_Pathway[,4])
   tteste<-ttest$estimate
   ttesp<-ttest$p.value
-  cat(i,tteste,ttesp,mwup,"\n",file=file.path(OUTPUT_PATH, "mONCTSG_Pathway_Result.txt"), append=TRUE)
+  mean_2ds <- c(mean_2ds, tteste['mean in group 2DS'])
+  mean_3do <- c(mean_3do, tteste['mean in group 3DO'])
+  tps <- c(tps, ttesp)
+  mwups <- c(mwups, mwup)
 }
+ttest_df <- data.frame(list(mean_2DS=mean_2ds,mean_3DO=mean_3do))
+ttest_df$t_p <- tps
+ttest_df$MWU_p <- mwups
+ttest_df$log10P <- -log10(ttest_df$MWU_p)
+ttest_df$MWU_q <- p.adjust(ttest_df$MWU_p, method='BH')
+ttest_df$significance <- ttest_df$MWU_q < 0.05
+rownames(ttest_df) <- colnames(mONCTSG_Pathway)[5:ncol(mONCTSG_Pathway)]
+ttest_df <- ttest_df[-which(rownames(ttest_df) == 'Grand.Average'), ]
+ttest_df$oncTSG <- ifelse(grepl('ONC', rownames(ttest_df)), 'ONC', 'TSG')
+rownames(ttest_df) <- gsub('TGF\\.beta', 'TGF-beta', rownames(ttest_df))
+rownames(ttest_df) <- gsub('RTK\\.RAS', 'RTK/RAS', rownames(ttest_df))
+rownames(ttest_df) <- gsub('\\.', ' ', rownames(ttest_df))
+ttest_df <- ttest_df[c('oncTSG', 'mean_2DS', 'mean_3DO', 't_p', 'MWU_p', 'log10P', 'MWU_q', 'significance')]
+ttest_df <- ttest_df[order(ttest_df$MWU_q), ]
+write.csv(ttest_df, file.path(OUTPUT_PATH, "mONCTSG_Pathway_Result.csv"), quote=FALSE)
 
-mOT_p<-read.csv(file.path(INPUT_PATH, "mONCTSG_p.csv"))
 
-mOT_gene_q<-p.adjust(mOT_p$gene, method="BH")
-mOT_mutfree_q<-p.adjust(mOT_p$mutfree, method="BH")
-mOT_pathway_q<-p.adjust(mOT_p$pathway, method="BH")
-write.csv(mOT_gene_q,file.path(OUTPUT_PATH, "mOT_gene_q.csv"))
-write.csv(mOT_mutfree_q,file.path(OUTPUT_PATH, "mOT_mutfree_q.csv"))
-write.csv(mOT_pathway_q,file.path(OUTPUT_PATH, "mOT_pathway_q.csv"))
-
-mOT_gene2<-read.csv(file.path(INPUT_PATH, "mONCTSG_Gene_Result2.csv"))
-mOT_pathway<-read.csv(file.path(INPUT_PATH, "mONCTSG_Pathway_Result.csv"))
+mOT_gene2<-read.csv(file.path(OUTPUT_PATH, "mONCTSG_Gene_Result2.csv"))
+mOT_pathway<-read.csv(file.path(OUTPUT_PATH, "mONCTSG_Pathway_Result.csv"))
 
 ggplot(mOT_gene2,aes(x=mean_2DS,y=mean_3DO))+geom_point(shape=21, alpha=0.5, aes(fill=oncTSG,colour=oncTSG,size=log10P))+scale_color_manual(values=c("#931751","#005492"))+scale_fill_manual(values=c("#931751","#005492"))+scale_alpha_manual(values=c(0.6,0.3,0.3))+scale_size(range=c(4,16))+theme(aspect.ratio=1)+xlim(-2.2,0.4)+ylim(-2.2,0.4)
 
